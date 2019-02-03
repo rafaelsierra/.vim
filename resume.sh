bc85b188cdf53f4d1304c8434ba60335dec5b49d
Rafael Sierra <rafaeljsg14@gmail.com>
2019-02-03 22:28:05 +0100

    Cleaning everything to start spacevim

--- resume.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run --rm -it --entrypoint /usr/sbin/sshd -v /:/host -p 127.0.0.1:60022:22 vim:snapshot -D
--- start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run --rm -it -v /:/host -p 127.0.0.1:60022:22 -a stdin -a stdout vim
